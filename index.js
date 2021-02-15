f665b79900c524dcf25f9029d67b4387f6c036ec
Jesse Lentz <jesselentz@gmail.com>
2021-02-15 12:32:26 -0500

    Check 4 closest stores

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
       await page.goto('https://www.riteaid.com/pharmacy/covid-qualifier', { waitUntil: 'domcontentloaded' });
       await page.waitForSelector('#dateOfBirth', { timeout: 10000, visible: true });
 
-      await page.type('#dateOfBirth', '08/25/1948');
+      await page.type('#dateOfBirth', '08/20/1948');
       await page.click('#Occupation');
       await page.click('#noneoftheabove');
       await page.type('#city', 'Berwyn');
@@ -46,29 +46,36 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
       const findStoreBtn = await page.waitForSelector('#btn-find-store');
       await findStoreBtn.click();
       await page.waitForSelector('.covid-store__result', { visible: true });
-      const selectStoreBtn = await page.waitForXPath('//a[contains(text(), "SELECT THIS STORE")]');
-      await selectStoreBtn.click();
-      await page.click('#continue');
-      try {
-        await page.waitForSelector('.covid-scheduler__invalid');
-      } catch(err) {
-        // Error message not found - possible availability
-        console.log(new Date().toLocaleString(), 'Appointments found!');
-        const screenshot = await page.screenshot({ encoding: 'base64' });
-        await sgMail.send({
-          attachments: [{
-            filename: 'screenshot.png',
-            type: 'image/png',
-            content_id: 'screenshot',
-            content: screenshot,
-            disposition: 'inline',
-          }],
-          from: 'Vaccine Notifier <fantasynotify@mailinator.com>',
-          html: `https://www.riteaid.com/pharmacy/covid-qualifier<br /><br /><img src="cid:screenshot" />`,
-          subject: `Rite Aid vaccine appointments may be available!`,
-          to: process.env.EMAILS.split(','),
-        });
-        process.exit();
+      const stores = (await page.$$('.covid-store__store__content')).slice(0, 4);
+      debugger
+      for (const store of stores) {
+        const storeName = await store.$eval('.covid-store__store__head span', (node) => node.innerText);
+        console.log(new Date().toLocaleString(), 'Checking', storeName);
+        const selectStoreBtn = await store.$('a.covid-store__store__anchor--unselected');
+        await selectStoreBtn.click();
+        await page.waitForTimeout(2000);
+        await page.click('#continue');
+        try {
+          await page.waitForSelector('.covid-scheduler__invalid');
+        } catch(err) {
+          // Error message not found - possible availability
+          console.log(new Date().toLocaleString(), `Appointments found at ${storeName}!`);
+          const screenshot = await page.screenshot({ encoding: 'base64' });
+          await sgMail.send({
+            attachments: [{
+              filename: 'screenshot.png',
+              type: 'image/png',
+              content_id: 'screenshot',
+              content: screenshot,
+              disposition: 'inline',
+            }],
+            from: 'Vaccine Notifier <fantasynotify@mailinator.com>',
+            html: `https://www.riteaid.com/pharmacy/covid-qualifier<br /><br />${storeName}<br /><br /><img src="cid:screenshot" />`,
+            subject: `Rite Aid vaccine appointments available at ${storeName}!`,
+            to: process.env.EMAILS.split(','),
+          });
+          process.exit();
+        }
       }
 
       await page.waitForTimeout(30000);
